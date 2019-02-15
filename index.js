3de18d591fc3769b558962894c6d77b9cea740e8
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-15 23:03:41 +0800

    update get posts, comments, albums, photos, todos

--- index.js
+++ index.js
@@ -3,8 +3,7 @@ const {
     gql
 } = require('apollo-server');
 const axios = require("axios");
-
-const MAIN_API_URL = "https://json-server-jsonplaceholder.herokuapp.com"
+const MAIN_API_URL = "https://json-server-jsonplaceholder.herokuapp.com";
 
 const typeDefs = gql `
     type Geo {
@@ -33,8 +32,44 @@ const typeDefs = gql `
         address: Address
         company: Company
     }
+    type Post {
+        userId: ID
+        id: ID
+        title: String
+        body: String
+    }
+    type Comment {
+        postId: ID
+        id: ID
+        name: String
+        email: String
+        body: String
+    }
+    type Album {
+        userId: ID
+        id: ID
+        title: String
+    }
+    type Photo {
+        albumId: ID
+        id: ID
+        title: String
+        url: String
+        thumbnailUrl: String
+    }
+    type Todo {
+        userId: ID
+        id: ID
+        title: String
+        completed: Boolean
+    }
     type Query {
         users: [User]
+        posts: [Post]
+        comments: [Comment]
+        albums: [Album]
+        photos: [Photo]
+        todos: [Todo]
     }
 `;
 
@@ -42,6 +77,21 @@ const resolvers = {
     Query: {
         users: () => {
             return axios(`${MAIN_API_URL}/users`).then(res => res.data)
+        },
+        posts: () => {
+            return axios(`${MAIN_API_URL}/posts`).then(res => res.data)
+        },
+        comments: () => {
+            return axios(`${MAIN_API_URL}/comments`).then(res => res.data)
+        },
+        albums: () => {
+            return axios(`${MAIN_API_URL}/albums`).then(res => res.data)
+        },
+        photos: () => {
+            return axios(`${MAIN_API_URL}/photos`).then(res => res.data)
+        },
+        todos: () => {
+            return axios(`${MAIN_API_URL}/todos`).then(res => res.data)
         },
     },
 };
