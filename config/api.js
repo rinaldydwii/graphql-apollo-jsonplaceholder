89ba8b6c356c64346fd6daa2a5cde808940e876d
Rinaldy Dwi Istanto <rinaldydwii@gmail.com>
2019-02-19 11:27:44 +0800

    create api config

---
+++ config/api.js
@@ -0,0 +1,18 @@
+// const MAIN_API_URL = "https://json-server-jsonplaceholder.herokuapp.com/"
+const MAIN_API_URL = "http://192.168.1.66:3000/"
+const USER_ROUTE = "users";
+const POST_ROUTE = "posts";
+const COMMENT_ROUTE = "comments";
+const ALBUM_ROUTE = "albums";
+const PHOTO_ROUTE = "photos";
+const TODO_ROUTE = "todos";
+
+module.exports = {
+    MAIN_API_URL,
+    USER_ROUTE,
+    POST_ROUTE,
+    COMMENT_ROUTE,
+    ALBUM_ROUTE,
+    PHOTO_ROUTE,
+    TODO_ROUTE
+}
